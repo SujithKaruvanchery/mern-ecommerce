8e9804acd0d3465bd41693a7d453618bdacb9bfb
sujith k <sujithkaruvanchery@gmail.com>
2025-01-08 22:40:49 +0530

    Add admin login functionality with routes

--- controller/adminControllers.js
+++ controller/adminControllers.js
@@ -1,5 +1,6 @@
 const AdminDB = require('../model/adminModel')
 const bcrypt = require('bcrypt');
+const {generateToken} = require('../utils/token')
 
 const registerAdmin = async (req, res) => {
     try {
@@ -39,4 +40,42 @@ const registerAdmin = async (req, res) => {
     }
 };
 
-module.exports = {registerAdmin}
+const loginAdmin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: 'All fields must be filled out.' });
+        }
+
+        const admin = await AdminDB.findOne({ email });
+        console.log(admin, "=======Admin Data");
+
+        if (!admin) {
+            return res.status(400).json({ error: 'No admin found with the provided details' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, admin.password);
+        console.log(passwordMatch, "========Password Match");
+
+        if (!passwordMatch) {
+            return res.status(400).json({ error: 'Password is incorrect' });
+        }
+
+        if (!admin.isActive) {
+            return res.status(400).json({ error: 'Admin profile is currently inactive' });
+        }
+
+        const token = generateToken(admin, "admin");
+        console.log(token, "=======Token");
+
+        res.cookie("admin_token", token);
+
+        res.status(200).json({ message: 'Successfully logged in.', data: admin });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message || 'Internal Server Error '});
+    }
+};
+
+module.exports = {registerAdmin,loginAdmin}
--- routes/v1/adminRoutes.js
+++ routes/v1/adminRoutes.js
@@ -1,7 +1,8 @@
-const { registerAdmin } = require('../../controller/adminControllers')
+const { registerAdmin, loginAdmin } = require('../../controller/adminControllers')
 
 const adminRouter = require('express').Router()
 
 adminRouter.post('/signup', registerAdmin)
+adminRouter.post('/login', loginAdmin)
 
 module.exports = adminRouter
