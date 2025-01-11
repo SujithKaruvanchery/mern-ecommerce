5136178474f74cc988a81a9708af0a41952cf39c
sujith k <sujithkaruvanchery@gmail.com>
2025-01-11 20:37:43 +0530

    Create add to cart controller and route for adding product to cart

--- controller/cartControllers.js
+++ controller/cartControllers.js
@@ -19,4 +19,50 @@ const getCart = async (req, res) => {
     }
 }
 
-module.exports = {getCart}
+const addProductToCart = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        console.log('=======userid', userId);
+        const { productId } = req.body;
+        console.log('=======productid', productId);
+
+        if (!productId) {
+            return res.status(404).json({ message: 'Please Provide ProductId' });
+        }
+
+        const product = await ProductDB.findById(productId);
+        if (!product) {
+            return res.status(404).json({ message: 'No products found in the database' });
+        }
+        console.log('=======product', product);
+
+        let cart = await CartDB.findOne({ userId });
+        if (!cart) {
+            cart = new CartDB({ userId, products: [] });
+        }
+
+        const productExists = cart.products.some((item) => item.productId.equals(productId));
+        if (productExists) {
+            return res.status(400).json({ message: 'Product Already In Cart' });
+        }
+        console.log('=======productexists', productExists);
+
+        cart.products.push({
+            productId,
+            price: product.price
+        });
+
+        cart.calculateTotalPrice();
+
+        await cart.save();
+
+        res.status(200).json({ message: 'Product Added To Cart', cart });
+
+    } catch (error) {
+        console.log(error);
+        res.status(error.status || 500).json({ error: error.message || 'Internal Server Error' });
+    }
+};
+
+
+module.exports = {getCart,addProductToCart}
--- routes/v1/cartRoutes.js
+++ routes/v1/cartRoutes.js
@@ -1,10 +1,10 @@
-const { getCart } = require('../../controller/cartControllers')
+const { getCart, addProductToCart } = require('../../controller/cartControllers')
 const { userAuth } = require('../../middleware/userAuth')
 
 const cartRouter = require('express').Router()
 
 cartRouter.get('/get-cart', userAuth, getCart)
-// cartRouter.post('/add-to-cart', userAuth, addProductToCart)
+cartRouter.post('/add-to-cart', userAuth, addProductToCart)
 // cartRouter.delete('/remove-from-cart', userAuth, removeProductFromCart)
 // cartRouter.delete('/clear-cart', userAuth, clearCart)
 
