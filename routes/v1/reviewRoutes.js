3dc7cecb44b4f5656eedc370bd44b489ba109c46
sujith k <sujithkaruvanchery@gmail.com>
2025-01-12 14:13:10 +0530

    Add delete review functionality with controller and route

--- routes/v1/reviewRoutes.js
+++ routes/v1/reviewRoutes.js
@@ -6,7 +6,7 @@ const reviewRouter = require('express').Router()
 
 reviewRouter.get('/get-reviews/:productId', userAuth, getProductReview);
 reviewRouter.post('/add-product-review', userAuth, addReview);
-reviewRouter.delete('/delete-review', userAuth, deleteReview);
+reviewRouter.delete('/delete-review/:reviewId', userAuth, deleteReview);
 // reviewRouter.get('/get-avg-rating', userAuth, getAverageRating);
 
 module.exports = reviewRouter
