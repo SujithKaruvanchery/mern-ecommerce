648fbf17004ba7e614a9b7e6b75ec537c26661c6
sujith k <sujithkaruvanchery@gmail.com>
2025-05-20 13:10:05 +0530

    Make review fetch routes public

--- client/src/redux/features/userSlice.jsx
+++ client/src/redux/features/userSlice.jsx
@@ -1,3 +1,27 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   isUserAuth: false,
+//   userData: {},
+// };
+
+// export const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     saveUser: (state, action) => {
+//       state.isUserAuth = true;
+//       state.userData = action.payload;
+//     },
+//     clearUser: (state) => {
+//       (state.isUserAuth = false), (state.userData = {});
+//     },
+//   },
+// });
+
+// export const { saveUser, clearUser } = userSlice.actions;
+// export default userSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -14,10 +38,12 @@ export const userSlice = createSlice({
       state.userData = action.payload;
     },
     clearUser: (state) => {
-      (state.isUserAuth = false), (state.userData = {});
+      state.isUserAuth = false;
+      state.userData = {};
     },
   },
 });
 
 export const { saveUser, clearUser } = userSlice.actions;
 export default userSlice.reducer;
+
--- server/routes/v1/reviewRoutes.js
+++ server/routes/v1/reviewRoutes.js
@@ -4,9 +4,9 @@ const { userAuth } = require('../../middleware/userAuth');
 
 const reviewRouter = require('express').Router()
 
-reviewRouter.get('/get-reviews/:productId', userAuth, getProductReview);
+reviewRouter.get('/get-reviews/:productId', getProductReview);
 reviewRouter.post('/addOrUpdateReview', userAuth, addOrUpdateReview);
 reviewRouter.delete('/delete-review/:reviewId', userAuth, deleteReview);
-reviewRouter.get('/get-average-rating/:productId', userAuth, getAverageRating);
+reviewRouter.get('/get-average-rating/:productId', getAverageRating);
 
 module.exports = reviewRouter
